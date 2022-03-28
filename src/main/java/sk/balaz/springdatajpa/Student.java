ba2ce448b9aec4392f5adfa59b81f6240501dae7
Tomas Balaz <tomasba@amdocs.com>
2022-03-28 13:18:19 +0200

    Annotated Student class with @Column annotation

--- src/main/java/sk/balaz/springdatajpa/Student.java
+++ src/main/java/sk/balaz/springdatajpa/Student.java
@@ -15,21 +15,48 @@ public class Student {
             strategy = GenerationType.SEQUENCE,
             generator = "student_sequence"
     )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
 
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String firstName;
 
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String lastName;
 
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT",
+            unique = true
+    )
+    private String email;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
     private Integer age;
 
     public Student(Long id,
                    String firstName,
                    String lastName,
-                   Integer age) {
+                   String email, Integer age) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
         this.age = age;
     }
 
@@ -65,12 +92,17 @@ public class Student {
         this.age = age;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
                 ", age=" + age +
                 '}';
     }
