ad6b8d01b69a7f5bb76b021252c5f6ae547e3554
Tomas Balaz <tomasba@amdocs.com>
2022-03-30 13:30:26 +0200

    Basic examples of basic Spring Data JPA methods - find, delete, save

--- src/main/java/sk/balaz/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/sk/balaz/springdatajpa/SpringDataJpaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
@@ -16,12 +18,46 @@ public class SpringDataJpaApplication {
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
             Student maria = new Student(
-                    "Maria ",
+                    "Maria",
                     "Jones",
                     "maria.jones@amigoscode.edu",
                     21
             );
-            studentRepository.save(maria);
+
+            Student ahmed = new Student(
+                    "Ahmed",
+                    "Ali",
+                    "ahmed.ali@amigoscode.edu",
+                    18
+            );
+
+            System.out.println("Adding Maria and Ahmed");
+            studentRepository.saveAll(List.of(maria, ahmed));
+
+            System.out.println("Number od students: ");
+            System.out.println(studentRepository.count());
+
+            studentRepository
+                    .findById(2L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with ID 2 not found"));
+
+            studentRepository
+                    .findById(3L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with ID 3 not found"));
+
+            System.out.println("Select all students");
+            List<Student> students = studentRepository.findAll();
+            students.forEach(System.out::println);
+
+            System.out.println("delete maria");
+            studentRepository.deleteById(1L);
+
+            System.out.println("Number od students: ");
+            System.out.println(studentRepository.count());
         };
     }
 
