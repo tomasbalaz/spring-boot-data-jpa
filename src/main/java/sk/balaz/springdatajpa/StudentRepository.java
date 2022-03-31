8a71fae08437b2b890c97c9df989d9f1c7f439ce
Tomas Balaz <tomasba@amdocs.com>
2022-03-31 12:53:38 +0200

    Add Named query with @Param Annotation

--- src/main/java/sk/balaz/springdatajpa/StudentRepository.java
+++ src/main/java/sk/balaz/springdatajpa/StudentRepository.java
@@ -2,6 +2,7 @@ package sk.balaz.springdatajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,8 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age = ?2")
     List<Student> findStudentByFirstNameEqualsAndAgeGreaterThan(String firstName, Integer age);
 
-    @Query(value = "SELECT * FROM student WHERE first_name = ?1 AND age >= ?2 ", nativeQuery = true)
-    List<Student> findStudentByFirstNameEqualsAndAgeGreaterThanNative(String firstName, Integer age);
+    @Query(value = "SELECT * FROM student WHERE first_name = :firstName AND age >= :age ", nativeQuery = true)
+    List<Student> findStudentByFirstNameEqualsAndAgeGreaterThanNative(
+            @Param("firstName") String firstName,
+            @Param("age") Integer age);
 }
