c81e767e77a71b262c956531227f7db1ca74b426
Tomas Balaz <tomasba@amdocs.com>
2022-04-04 11:47:57 +0200

    Encapsulate sorting into method

--- src/main/java/sk/balaz/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/sk/balaz/springdatajpa/SpringDataJpaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
@@ -19,22 +21,35 @@ public class SpringDataJpaApplication {
         return args -> {
 
             Faker faker = new Faker();
+
             generateRandomStudents(studentRepository, faker);
 
-            //1. option
-            Sort sort = Sort.by(Sort.Direction.DESC, "firstName");
-            studentRepository.findAll(sort)
-                    .forEach(student -> System.out.println(student.getFirstName()));
+            sortStudents(studentRepository);
 
-            //2. option
-            Sort sort2 = Sort.by("firstName").ascending()
-                    .and(Sort.by("age").descending());
+            PageRequest pageRequest = PageRequest.of(
+                    0,
+                    5,
+                    Sort.by("firstName").ascending());
 
-            studentRepository.findAll(sort2)
-                    .forEach(student ->
-                            System.out.println(student.getFirstName() + " " + student.getAge()));
+            Page<Student> page = studentRepository.findAll(pageRequest);
 
+            System.out.println(page);
         };
+    }
+
+    private void sortStudents(StudentRepository studentRepository) {
+        //1. option
+        Sort sort = Sort.by(Sort.Direction.DESC, "firstName");
+        studentRepository.findAll(sort)
+                .forEach(student -> System.out.println(student.getFirstName()));
+
+        //2. option
+        Sort sort2 = Sort.by("firstName").ascending()
+                .and(Sort.by("age").descending());
+
+        studentRepository.findAll(sort2)
+                .forEach(student ->
+                        System.out.println(student.getFirstName() + " " + student.getAge()));
     }
 
     private void generateRandomStudents(StudentRepository studentRepository, Faker faker) {
